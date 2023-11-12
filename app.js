f0071bcc45b1ce67b7262358f25ec20df4f0103a
Mohammad <gramerbazarbd24@gmail.com>
2023-11-12 08:41:33 +0600

    ayah display uthmani script

--- app.js
+++ app.js
@@ -5,11 +5,22 @@ const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
+// Function to fetch ayahs based on surah number
+async function fetchAyahs(chapterNumber) {
+  try {
+    const response = await axios.get(`https://api.quran.com/api/v4/quran/verses/uthmani?chapter_number=${chapterNumber}`);
+    return response.data.verses || [];
+  } catch (error) {
+    console.error('Error fetching ayahs:', error.message);
+    throw error;
+  }
+}
+
 app.get('/', async (req, res) => {
   try {
     // Fetch surahs from the Quran API
-    const response = await axios.get('https://api.quran.com/api/v4/chapters');
-    const surahs = response.data.chapters || []; // Use an empty array if chapters is not defined
+    const surahResponse = await axios.get('https://api.quran.com/api/v4/chapters');
+    const surahs = surahResponse.data.chapters || [];
 
     // Render the main page with the list of surahs
     res.render('index', { surahs });
@@ -18,6 +29,24 @@ app.get('/', async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+
+app.get('/surah/:number', async (req, res) => {
+    const surahNumber = req.params.number;
+    try {
+      // Fetch specific surah data from the API
+      const surahResponse = await axios.get(`https://api.quran.com/api/v4/chapters/${surahNumber}`);
+      const surah = surahResponse.data || {}; // Access the data directly
+  
+      // Fetch ayahs for the selected surah
+      const ayahs = await fetchAyahs(surahNumber);
+  
+      // Render the surah page with verses
+      res.render('surah', { surah, ayahs });
+    } catch (error) {
+      console.error(`Error fetching Surah ${surahNumber} data:`, error.message);
+      res.status(500).send('Internal Server Error');
+    }
+  });  
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
