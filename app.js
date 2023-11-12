f546317e30118f43d04dbf85e0e01e8f6942e743
Mohammad <gramerbazarbd24@gmail.com>
2023-11-12 08:19:24 +0600

    api changed

--- app.js
+++ app.js
@@ -7,33 +7,18 @@ app.set('view engine', 'ejs');
 
 app.get('/', async (req, res) => {
   try {
-    // Fetch Quranic data from the API
-    const response = await axios.get('https://api.alquran.cloud/v1/quran/en.sahih');
-    const surahs = response.data.data.surahs;
+    // Fetch surahs from the Quran API
+    const response = await axios.get('https://api.quran.com/api/v4/chapters');
+    const surahs = response.data.chapters || []; // Use an empty array if chapters is not defined
 
     // Render the main page with the list of surahs
     res.render('index', { surahs });
   } catch (error) {
-    console.error('Error fetching Quranic data:', error.message);
+    console.error('Error fetching surahs:', error.message);
     res.status(500).send('Internal Server Error');
   }
 });
 
-app.get('/surah/:number', async (req, res) => {
-    const surahNumber = req.params.number;
-    try {
-      // Fetch specific surah data from the API
-      const response = await axios.get(`https://api.alquran.cloud/v1/surah/${surahNumber}`);
-      const surah = response.data.data;
-
-      // Render the surah page
-      res.render('surah', { surah });
-    } catch (error) {
-      console.error(`Error fetching Surah ${surahNumber} data:`, error.message);
-      res.status(500).send('Internal Server Error');
-    }
-  });
-  
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
